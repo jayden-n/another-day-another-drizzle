f621f11628ee1743b025a3676238c7420ddf2245
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-08-10 16:44:39 -0400

    added: openWeather API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
-const App = () => {
+import { ChangeEvent, useState } from "react";
+
+const App: React.FC = (): JSX.Element => {
+  const [term, setTerm] = useState<string>("");
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        process.env.REACT_APP_API_KEY
+      }`,
+    );
+  };
+
+  // the expected event type is a change event on an HTML input element.
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+
+    if (value === "") return;
+    getSearchOptions(value);
+  };
+
   return (
     <main className="flex h-[100vh] w-full items-center justify-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400">
       <section className="flex h-full w-full flex-col items-center justify-center rounded bg-white bg-opacity-20 p-4 text-center text-zinc-700 drop-shadow-lg backdrop-blur-lg md:max-w-[500px] md:px-10 lg:h-[500px] lg:p-24">
@@ -12,8 +33,9 @@ const App = () => {
         <div className="mt-10 flex md:mt-4 ">
           <input
             type="text"
-            value={"X"}
+            value={term}
             className="rounded-l-md border-2 border-white px-2 py-1"
+            onChange={onInputChange}
           />
           <button className="cursor-pointer rounded-r-md border-2 border-zinc-100 px-2 py-1 text-zinc-100 hover:border-zinc-500 hover:text-zinc-500 ">
             Search
