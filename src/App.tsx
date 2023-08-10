a4b62abd9fe5270e0631d7cfb30fa72182cad83b
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-08-10 18:17:33 -0400

    implemented: 2nd api call to get weather forecast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { OptionType } from "./components/types";
 const App: React.FC = (): JSX.Element => {
   const [term, setTerm] = useState<string>("");
+  const [city, setCity] = useState<OptionType | null>(null);
   const [options, setOptions] = useState<[]>([]);
 
   const getSearchOptions = (value: string) => {
@@ -23,13 +24,30 @@ const App: React.FC = (): JSX.Element => {
     getSearchOptions(value);
   };
 
-  const onOptionSelect = (option: OptionType) => {
-    console.log(option.name);
-
+  const getForecast = (city: OptionType) => {
     fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${process.env.REACT_APP_API_KEY}`,
-    );
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log({ data }));
   };
+
+  const onSubmit = () => {
+    if (!city) return;
+    getForecast(city);
+  };
+
+  const onOptionSelect = (option: OptionType) => {
+    setCity(option);
+  };
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      // Reset the UI if users chose a city
+      setOptions([]);
+    }
+  }, [city]);
   return (
     <main className="flex h-[100vh] w-full items-center justify-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400">
       <section className="flex h-full w-full flex-col items-center justify-center rounded bg-white bg-opacity-20 p-4 text-center text-zinc-700 drop-shadow-lg backdrop-blur-lg md:max-w-[500px] md:px-10 lg:h-[500px] lg:p-24">
@@ -59,7 +77,10 @@ const App: React.FC = (): JSX.Element => {
               </li>
             ))}
           </ul>
-          <button className="cursor-pointer rounded-r-md border-2 border-zinc-100 px-2 py-1 text-zinc-100 hover:border-zinc-500 hover:text-zinc-500 ">
+          <button
+            className="cursor-pointer rounded-r-md border-2 border-zinc-100 px-2 py-1 text-zinc-100 hover:border-zinc-500 hover:text-zinc-500"
+            onClick={onSubmit}
+          >
             Search
           </button>
         </div>
